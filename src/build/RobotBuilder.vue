3a665a5fd47f4a71a1581f1df37f1de838fa9eec
Luis Carbuccia <lcarbuccia@gmail.com>
2021-01-23 15:46:17 -0500

    minor refactoring

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <button class="add-to-cart" @click="addToCard()"> Add to cart </button>
         <div class="top-row">
-            <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}">
+            <div :class="[saleBorderClass, 'top', 'part']">
                 <div class="robot-name">
                     {{selectedRobot.head.title}}
                     <span v-show="selectedRobot.head.onSale" clase="sale"> Sale! </span>
@@ -83,6 +83,9 @@ export default {
     };
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
     headBorderStyle() {
       return { border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid gray' };
     },
